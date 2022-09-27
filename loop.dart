a5fa9b9daf22957bcacbf50327535db4b7ce43a6
xclusiveskate <abdullahiafolabi08@gmail.com>
2022-09-27 04:30:50 -0700

    new commit

--- loop.dart
+++ loop.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   //  var sum = 10;
@@ -24,6 +25,27 @@ void main() {
   //   print(planets[i]);
   // }
 
+  // var fruit = {1: 'Apple', 2: 'Banana', 3: 'Cherry', 4: 'Orange'};
+  // for (var key in fruit.keys) {
+  //   print(key);
+  // }
+  // for (var value in fruit.values) {
+  //   print(value);
+  // }
+  // for (var e in fruit.entries) {
+  //   print("${e.key}:${e.value}");
+  // }
+
+  // var a1 = ["A", "B", "C"];
+  // var a2 = ["A", "B", "C"];
+  // var a3 = ["A", "B", "C"];
+  // for (var i = 0; i < a1.length; i++) {
+  //   for (var j = 0; j < a2.length; j++){
+  //     for (var k = 0; k < a3.length; k++) {
+  //       print(a1[i] + a2[j] + a3[k]);
+  //     }
+  //   }
+  // }
   // var list = [2, 3, 4, 5, 3, 45, 67, 88, 555, 545, 67]; //print even and odd
   // var a = list;
   // for (var i = 0; i < list.length; i++) {
@@ -37,6 +59,15 @@ void main() {
   //   }
   // }
 
+  // const int MAX = 10;
+  // while (true) {
+  //   var num =  Random().nextInt(MAX);
+  //   print("$num");
+  //     if(num == 8) {
+  //       break;
+  //     }
+  //   print("\n");
+  // }
   //  var list = [ [[1, 2],[3, 4]],[[5, 6],[7, 8]],[[9, 10],[11, 12]]];
   //   for (var i = 0; i < list.length; i++) {
   //     var numb = list[i];
@@ -49,6 +80,13 @@ void main() {
   //       }
   //     }
   //   }
+
+  // var num = 5;
+  // var factorial = 1;
+  // for (var i = num; i >= 1; i--) {
+  //   factorial *= i;
+  // }
+  // print(factorial);
 
   // var lis1 = [[[2, 4, 6],[3]], 3];
 // for (var i = 0; i < lis1.length; i++) {
@@ -64,9 +102,6 @@ void main() {
 //     print(lis1[i]);
 //   }
 // }
-
-
-
 
   // var lis = [ [[2, 4, 6],[3]], [[3, 5],[3,5]] ];
   // for (var i = 0; i < lis.length; i++) {
@@ -106,7 +141,7 @@ void main() {
   //   count--;
   // }
 
- //FOR FOR FOR FOR
+  //FOR FOR FOR FOR
   // var counter = 0;
   // for (var i = 0; i <= 2; i++) {
   //   print(i);
@@ -126,7 +161,7 @@ void main() {
   // }
 
   // var lis3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  //FOR FOR FOR  
+  //FOR FOR FOR
   //   for (var i = 0; i < lis3.length; i++) {
   //     if (lis3[i] % 2 == 0) {
   //       print(lis3[i] * 2);
@@ -155,10 +190,9 @@ void main() {
   //   }
   // }
 
-
-///ASSIGNMENT:
-///1.  // var list = [2, 3, 4, 5, 3, 45, 67, 88, 555, 545, 67]; //print even and odd
-///2. var list = [ [[1, 2],[3, 4]],[[5, 6],[7, 8]],[[9, 10],[11, 12]]];print the thing in the list
-///3. var lis = [ [[2, 4, 6],[3]], [[3, 5],[3,5]] ]; 
-///4.var lis1 = [[[2, 4, 6],[3]], 3]; 
+  ///ASSIGNMENT:
+  ///1.  // var list = [2, 3, 4, 5, 3, 45, 67, 88, 555, 545, 67]; //print even and odd
+  ///2. var list = [ [[1, 2],[3, 4]],[[5, 6],[7, 8]],[[9, 10],[11, 12]]];print the thing in the list
+  ///3. var lis = [ [[2, 4, 6],[3]], [[3, 5],[3,5]] ];
+  ///4.var lis1 = [[[2, 4, 6],[3]], 3];
 }
